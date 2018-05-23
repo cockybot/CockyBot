5151992d792c3ebb4e3bca62bc51d9faee31d9f6
CockyBot <39292039+cockybot@users.noreply.github.com>
2018-05-22 18:07:11 -0700

    Place credentials in separate file

---
+++ twitter-credentials.php
@@ -0,0 +1,10 @@
+<?php
+
+// TWITTER CREDENTIALS – NEVER PLACE THIS FILE IN A PUBLIC LOCATION!
+// Get yours from: https://apps.twitter.com/
+// Note: You must replace the Xs with your own credentials before you can tweet.
+
+$consumerKey = "XXXXXXXXXXXXXXXXXXXXXXXXX";
+$consumerSecret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+$accessToken = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+$accessTokenSecret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
